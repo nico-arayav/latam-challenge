c8b33fa4977fae0e1b4a55d7d20093c6e55a3253
Nicolás Araya Valdés <nico.arayav@gmail.com>
2024-10-17 14:03:31 -0300

    Create PreProcessing class

--- challenge/model.py
+++ challenge/model.py
@@ -9,33 +9,13 @@ class DelayModel:
         self
     ):
         self._model = None # Model should be saved in this attribute.
-
-    def get_period_day(date):
-        date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
-        morning_min = datetime.strptime("05:00", '%H:%M').time()
-        morning_max = datetime.strptime("11:59", '%H:%M').time()
-        afternoon_min = datetime.strptime("12:00", '%H:%M').time()
-        afternoon_max = datetime.strptime("18:59", '%H:%M').time()
-        evening_min = datetime.strptime("19:00", '%H:%M').time()
-        evening_max = datetime.strptime("23:59", '%H:%M').time()
-        night_min = datetime.strptime("00:00", '%H:%M').time()
-        night_max = datetime.strptime("4:59", '%H:%M').time()
-
-        if(date_time > morning_min and date_time < morning_max):
-            return 'mañana'
-        elif(date_time > afternoon_min and date_time < afternoon_max):
-            return 'tarde'
-        elif(
-            (date_time > evening_min and date_time < evening_max) or
-            (date_time > night_min and date_time < night_max)
-        ):
-            return 'noche'
+        self.preprocessor = DataPreprocessor()
 
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -49,12 +29,13 @@ class DelayModel:
             pd.DataFrame: features.
         """
         # Create the 'period_day' feature
-        data['period_day'] = data['Fecha-I'].apply(self.get_period_day)
+        data['period_day'] = data['Fecha-I'].apply(self.preprocessor.get_period_day)
 
         if target_column:
             X = data.drop(columns=[target_column])
             y = data[target_column]
             return X, y
+        
         return data
 
     def fit(
@@ -85,3 +66,27 @@ class DelayModel:
             (List[int]): predicted targets.
         """
         return
+    
+class DataPreprocessor:
+
+    @staticmethod
+    def get_period_day(date):
+        date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+        morning_min = datetime.strptime("05:00", '%H:%M').time()
+        morning_max = datetime.strptime("11:59", '%H:%M').time()
+        afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+        afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+        evening_min = datetime.strptime("19:00", '%H:%M').time()
+        evening_max = datetime.strptime("23:59", '%H:%M').time()
+        night_min = datetime.strptime("00:00", '%H:%M').time()
+        night_max = datetime.strptime("4:59", '%H:%M').time()
+
+        if(date_time > morning_min and date_time < morning_max):
+            return 'mañana'
+        elif(date_time > afternoon_min and date_time < afternoon_max):
+            return 'tarde'
+        elif(
+            (date_time > evening_min and date_time < evening_max) or
+            (date_time > night_min and date_time < night_max)
+        ):
+            return 'noche'
