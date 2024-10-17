06c3f012dafbe9da27d304cea85c7a78c089caa5
Nicolás Araya Valdés <nico.arayav@gmail.com>
2024-10-17 14:09:46 -0300

    Add min_diff feature

--- challenge/model.py
+++ challenge/model.py
@@ -31,6 +31,7 @@ class DelayModel:
         # Create the 'period_day' feature
         data['period_day'] = data['Fecha-I'].apply(self.preprocessor.get_period_day)
         data['high_season'] = data['Fecha-I'].apply(self.preprocessor.is_high_season)
+        data['min_diff'] = data.apply(self.preprocessor.get_min_diff, axis=1)
 
         if target_column:
             X = data.drop(columns=[target_column])
@@ -112,3 +113,10 @@ class DataPreprocessor:
             return 1
         else:
             return 0
+        
+    @staticmethod
+    def get_min_diff(data):
+        fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds())/60
+        return min_diff
