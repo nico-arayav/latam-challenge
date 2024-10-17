1a1f56e6e67ace555cb18647be1ac821b985dad7
Nicolás Araya Valdés <nico.arayav@gmail.com>
2024-10-17 14:05:53 -0300

    Add high_season feature

--- challenge/model.py
+++ challenge/model.py
@@ -30,6 +30,7 @@ class DelayModel:
         """
         # Create the 'period_day' feature
         data['period_day'] = data['Fecha-I'].apply(self.preprocessor.get_period_day)
+        data['high_season'] = data['Fecha-I'].apply(self.preprocessor.is_high_season)
 
         if target_column:
             X = data.drop(columns=[target_column])
@@ -90,3 +91,24 @@ class DataPreprocessor:
             (date_time > night_min and date_time < night_max)
         ):
             return 'noche'
+        
+    @staticmethod
+    def is_high_season(fecha):
+        fecha_año = int(fecha.split('-')[0])
+        fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+        range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
+        range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
+        range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
+        range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
+        range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
+        range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
+        range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
+        range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
+
+        if ((fecha >= range1_min and fecha <= range1_max) or 
+            (fecha >= range2_min and fecha <= range2_max) or 
+            (fecha >= range3_min and fecha <= range3_max) or
+            (fecha >= range4_min and fecha <= range4_max)):
+            return 1
+        else:
+            return 0
